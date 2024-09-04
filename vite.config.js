d40040aee7fe9903c25cc6bb74be3e50282c2ef3
Harish <68741486+harish-x@users.noreply.github.com>
2024-09-04 10:56:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1600,
+  build: { chunkSizeWarningLimit: 1600 },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: true,
   },
 });
